3b7f9dd6874eee3ca82d4a82812915d9a289983a
nidhisingh24 <nidhisi@infinite.com>
2024-02-28 12:50:52 +0530

    Test

---
+++ Test/Test1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to change");
+            
+            string inputStr = Console.ReadLine();
+            
+            if(inputStr.Length>=2)
+            {
+                char firstWord = inputStr[0];
+                char lastWord = inputStr[inputStr.Length - 1];
+                string finalStr = lastWord + inputStr.Substring(1, inputStr.Length - 2) + firstWord;
+                Console.WriteLine("Final String is" +" "+ finalStr);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Inorder to change string it must contain two charcters");
+            }
+        }
+    }
+}
---
+++ Test/Test2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(1, 2, 3));
+            Console.WriteLine(test(1, 3, 2));
+            Console.WriteLine(test(1, 1, 1));
+            Console.WriteLine(test(1, 2, 2));
+            Console.ReadLine();
+
+        }
+        public static int test(int a, int b, int c)
+        {
+            var max = Math.Max(a, Math.Max(b, c));
+            return max;
+        }
+    }
+}
